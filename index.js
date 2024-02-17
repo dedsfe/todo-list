caccb3b6dce0b9fba035fff120eef4b9590c2d25
dedsfe <111099027+dedsfe@users.noreply.github.com>
2024-02-17 00:32:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,63 +2,66 @@ $(document).ready(function() {
     var $input = $('.input');
     var $button = $('.btn');
     var $lista = $('.todo');
-    
+  
     // Carregar tarefas salvas do localStorage
     var toDosAtuais = JSON.parse(localStorage.getItem('todos')) || [];
-
+  
     // Atualizar a lista de tarefas quando a página é carregada
     updateList();
-
+  
     $input.on("keypress", function(event) {
-        if (event.key === "Enter") {
-            var inputValue = $input.val();
-            if (inputValue !== "") {
-                toDosAtuais.push(inputValue);
-                $input.val('');
-                updateList();
-                saveToLocalStorage(); // Salvar após adicionar
-            } else {
-                console.log('Está Vazio o Input');
-            }
-        }
-    });
-
-    // Função para adicionar itens à lista
-    $button.on("click", function() {
+      if (event.key === "Enter") {
         var inputValue = $input.val();
         if (inputValue !== "") {
-            toDosAtuais.push(inputValue);
-            $input.val('');
-            updateList();
-            saveToLocalStorage(); // Salvar após adicionar
+          toDosAtuais.push(inputValue);
+          $input.val('');
+          updateList();
+          saveToLocalStorage(); // Salvar após adicionar
         } else {
-            console.log('Está Vazio o Input');
+          console.log('Está Vazio o Input');
         }
+      }
     });
-
+  
+    // Função para adicionar itens à lista
+    $button.on("click", function() {
+      var inputValue = $input.val();
+      if (inputValue !== "") {
+        toDosAtuais.push(inputValue);
+        $input.val('');
+        updateList();
+        saveToLocalStorage(); // Salvar após adicionar
+      } else {
+        console.log('Está Vazio o Input');
+      }
+    });
+  
     // Função para atualizar a lista
     function updateList() {
-        $lista.empty();
-        $.each(toDosAtuais, function(index, item) {
-            var $textoDaLista = $('<li>').text(item);
-            var $botaoApagar = $('<button>').text('Apagar').addClass('botao_apagar');
-            
-            $botaoApagar.on("click", function() {
-                toDosAtuais.splice(index, 1);
-                updateList();
-                saveToLocalStorage(); // Salvar após remover
-            });
-            
-            $textoDaLista.append($botaoApagar);
-            $lista.append($textoDaLista);
+      $lista.empty();
+      $.each(toDosAtuais, function(index, item) {
+        var $textoDaLista = $('<li>').text(item);
+        var $botaoApagar = $('<button>').text('Apagar').addClass('botao_apagar');
+  
+        $botaoApagar.on("click", function() {
+          $(this).parent().fadeOut(500, function() {
+            toDosAtuais.splice(index, 1);
+            updateList();
+            saveToLocalStorage(); // Salvar após remover
+          });
         });
+  
+        $textoDaLista.append($botaoApagar);
+        $lista.append($textoDaLista);
+      });
     }
-
+  
     // Função para salvar a lista no localStorage
     function saveToLocalStorage() {
-        localStorage.setItem('todos', JSON.stringify(toDosAtuais));
+      localStorage.setItem('todos', JSON.stringify(toDosAtuais));
     }
-
+  
     // Inicializa a lista com os dados do localStorage
     updateList();
-});
+  });
+  
